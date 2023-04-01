8a900b0e72ae373f01211160f18400b01f6093c7
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-04-01 15:28:44 +0530

    Hm

--- plugins/_main.js
+++ plugins/_main.js
@@ -60,7 +60,7 @@ Description : ${i.desc}\`\`\``
        });
       cmnd.sort();
       category.sort().forEach((cmmd) => {
-       menu += `╔════════════࿇\n║\n║◤${cmmd}◢\n`
+       menu += `╔════════════࿇\n║\n║◤${cmmd}◢\n║\n`
         let comad = cmnd.filter(({ type }) => type == cmmd);
         comad.forEach(({ cmd }, num) => {
           menu += `║✠${(num += 1)} ${cmd.trim()}\n`;
