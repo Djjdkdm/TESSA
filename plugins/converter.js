9575fb782519daaa831d2f876036ba9d6faaa789
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2023-04-01 19:19:20 +0530

    🥰

--- plugins/converter.js
+++ plugins/converter.js
@@ -1,25 +1,20 @@
-const {
-  command,
-  qrcode,
-  webp2mp4,
-  isUrl,
-  isPrivate,
-} = require("../lib/");
-const { yta, ytIdRegex, ytv } = require("../lib/yotube");
-const { search } = require("yt-search");
-const { toAudio } = require("../lib/media");
-let gis = require("g-i-s");
-const { AddMp3Meta } = require("../lib");
-const jimp = require("jimp");
-const QRReader = require("qrcode-reader");
-const { RMBG_KEY } = require("../config");
-let { unlink } = require("fs/promises");
-const got = require("got");
-const FormData = require("form-data");
-const stream = require("stream");
-const { promisify } = require("util");
-const pipeline = promisify(stream.pipeline);
-const fs = require("fs");
+const { command, qrcode, webp2mp4, isUrl, isPrivate, getJson, sleep, tiny } = require('../lib/')
+const { toAudio } = require('../lib/media')
+let gis = require('g-i-s')
+const { AddMp3Meta } = require('../lib')
+const jimp = require('jimp')
+const QRReader = require('qrcode-reader')
+const { RMBG_KEY } = require('../config')
+let { unlink } = require('fs/promises')
+const got = require('got')
+const FormData = require('form-data')
+const stream = require('stream')
+const { promisify } = require('util')
+const pipeline = promisify(stream.pipeline)
+const fs = require('fs')
+const config = require('../config')
+const { STICKER_DATA, AUDIO_DATA } = require('../config.js')
+const { Image } = require('node-webpmux')
 
 command(
   {
@@ -176,5 +171,97 @@ command(
     let buff = await m.quoted.download();
     buff = await toAudio(buff, "mp3");
     return await message.sendMessage(buff, { mimetype: "audio/mpeg" }, "audio");
+  }
+);
+command(
+  {
+    pattern: "sticker ?(.*)",
+    fromMe: isPrivate,
+    type: "converter",
+  },
+  async (message, match, m) => {
+    if (!(message.reply_message.video || message.reply_message.image))
+      return await message.reply("_Reply to photo or video_");
+    let buff = await m.quoted.download();
+    message.sendMessage(
+      buff,
+      { packname: config.STICKER_DATA.split(",")[0], author: config.STICKER_DATA.split(",")[1]},
+      "sticker"
+    );
+  }
+);
+
+command(
+  {
+    pattern: "tgs ?(.*)",
+    fromMe: isPrivate,
+    type: "converter",
+  },
+  async (message, match) => {
+    if (!match)
+      return message.reply(
+        "_Enter a tg sticker url_\nEg: https://t.me/addstickers/Oldboyfinal\nKeep in mind that there is a chance of ban if used frequently"
+      );
+    let packid = match.split("/addstickers/")[1];
+    let { result } = await getJson(
+      `https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(
+        packid
+      )}`
+    );
+    if (result.is_animated)
+      return message.reply("_Animated stickers are not supported_");
+    message.reply(
+      `*Total stickers :* ${result.stickers.length}\n*Estimated complete in:* ${
+        result.stickers.length * 1.5
+      } seconds`.trim()
+    );
+    for (let sticker of result.stickers) {
+      let file_path = await getJson(
+        `https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${sticker.file_id}`
+      );
+      await message.sendMessage(
+        `https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/${file_path.result.file_path}`,
+        { packname: config.STICKER_DATA.split(",")[0], author: config.STICKER_DATA.split(",")[1] },
+        "sticker"
+      );
+      sleep(1500);
+    }
+  }
+);
+
+command(
+  {
+    pattern: "take ?(.*)",
+    fromMe: isPrivate,
+    type: "converter",
+  },
+  async (message, match, m) => {
+    if (!message.reply_message && !message.reply_message.sticker)
+      return await message.reply("_Reply to sticker_");
+    let buff = await m.quoted.download();
+    let [packname, author] = match.split(",");
+    await message.sendMessage(
+      buff,
+      {
+        packname : packname || config.STICKER_DATA.split(",")[0], author : author || config.STICKER_DATA.split(",")[1]
+      },
+      "sticker"
+    );
+  }
+);
+
+command(
+  {
+    pattern: "getexif ?(.*)",
+    fromMe: true,
+    type: "converter",
+  },
+  async (message, match, m) => {
+    if (!message.reply_message || !message.reply_message.sticker)
+      return await message.reply("_Reply to sticker_");
+    let img = new Image();
+    await img.load(await m.quoted.download());
+    const exif = JSON.parse(img.exif.slice(22).toString());
+    await message.reply(exif);
   }
 );
